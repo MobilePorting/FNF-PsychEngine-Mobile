81e64468c23898ddefe71833c19f355ffd47dfbd
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2025-04-04 02:05:30 +0200

    Update ModsMenuState.hx

--- source/states/ModsMenuState.hx
+++ source/states/ModsMenuState.hx
@@ -319,9 +319,10 @@ class ModsMenuState extends MusicBeatState
 		#if mobile
 		addTouchPad('UP_DOWN', 'B');
 		touchPad.y -= 215; // so that you can press the buttons.
+		#end
+		
 		if (controls.mobileC)
 			touchPad.alpha = 0.3;
-		#end
 		
 		super.create();
 	}
